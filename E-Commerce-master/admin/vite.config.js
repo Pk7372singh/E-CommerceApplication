19eac29449d36e3c36f20ad9739e276228a5812b
Prafful Kumar <92020279+Pk7372singh@users.noreply.github.com>
2024-09-15 17:22:56 +0530

    Update vite.config.js

--- E-Commerce-master/admin/vite.config.js
+++ E-Commerce-master/admin/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5678,    // Specify the port you want to use
+    host: true     // Make Vite accessible from external networks
+  }
+});
